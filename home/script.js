059763809cb887bbe2d5cb9484ba203c1020af67
Soulra01 <raden.raharja@binus.ac.id>
2023-09-29 21:49:34 +0700

    developer 45%

---
+++ home/script.js
@@ -0,0 +1,24 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".menu");
+
+hamburgerMenu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+// Menambahkan efek dropdown pada submenu
+const submenuItems = document.querySelectorAll(".menu li.has-submenu");
+
+submenuItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        e.stopPropagation(); // Mencegah menu utama ditutup saat submenu diklik
+        item.classList.toggle("show-submenu");
+    });
+});
+
+document.addEventListener("click", (e) => {
+    submenuItems.forEach((item) => {
+        if (!item.contains(e.target)) {
+            item.classList.remove("show-submenu");
+        }
+    });
+});
